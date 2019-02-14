7e57c2ee25b92a5a4f2b6c2a9d378f5bd23c3f5e
ivanmir <ivan.mirisola@gmail.com>
2019-02-14 22:32:48 +0000

    numpy

--- src/python/supply/supply.go
+++ src/python/supply/supply.go
@@ -338,6 +338,7 @@ func (s *Supplier) InstallPipPop() error {
 }
 
 func (s *Supplier) InstallNumPy() error {
+	tempPath := filepath.Join("/tmp", "numpy")
         if err := s.Command.Execute(s.Stager.BuildDir(), ioutil.Discard, ioutil.Discard, "python", "-m", "pip", "install", "numpy scipy matplotlib", "--exists-action=w", "--no-index", fmt.Sprintf("--find-links=%s", tempPath)); err != nil {
         	s.Log.Debug("******Path val: %s", os.Getenv("PATH"))
 		s.Log.Debug("[Numpy Installation Error]: ", err)
